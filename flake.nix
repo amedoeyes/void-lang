7632abfd4d80ec2027b900e4684f4963e916ae8a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2025-06-29 05:32:36 +0300

    nix: use rust-overlay to get 1.88

--- flake.nix
+++ flake.nix
@@ -1,15 +1,24 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
+    {
+      nixpkgs,
+      rust-overlay,
+      flake-utils,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
       in
       {
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
@@ -37,8 +46,7 @@
 
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            rustc
-            cargo
+            rust-bin.stable.latest.default
           ];
         };
       }
