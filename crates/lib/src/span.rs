39c386b8fd90fb3e7aea3fa4b02716a5351b5524
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2026-07-03 17:02:06 +0300

    lib, span: add dummy and derive ord to clean up merge

--- crates/lib/src/span.rs
+++ crates/lib/src/span.rs
@@ -1,41 +1,46 @@
 use std::fmt::Debug;
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Position {
     pub line: usize,
     pub column: usize,
 }
 
 impl Position {
+    pub const DUMMY: Self = Self { line: 0, column: 0 };
+
     pub fn new(line: usize, column: usize) -> Self {
         Self { line, column }
     }
 }
 
-#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Span {
     pub start: Position,
     pub end: Position,
 }
 
 impl Span {
+    pub const DUMMY: Self = Self {
+        start: Position::DUMMY,
+        end: Position::DUMMY,
+    };
+
     pub fn new(start: Position, end: Position) -> Self {
         Self { start, end }
     }
 
     pub fn merge(self, other: Self) -> Self {
-        let mut span = self;
-        if other.start.line < span.start.line
-            || (other.start.line == span.start.line && other.start.column < span.start.column)
-        {
-            span.start = other.start;
+        if self == Self::DUMMY {
+            other
+        } else if other == Self::DUMMY {
+            self
+        } else {
+            Span {
+                start: self.start.min(other.start),
+                end: self.end.max(other.end),
+            }
         }
-        if other.end.line > span.end.line
-            || (other.end.line == span.end.line && other.end.column > span.end.column)
-        {
-            span.end = other.end;
-        }
-        span
     }
 }
 
